8d5fb50f0ce8ede46f83d2000f788d6a33e81260
Tiago Silva <tiago.toya@hotmail.com>
2020-03-19 14:07:30 -0300

    feat: hello world

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js')
+
+const bot = new Discord.Client()
+
+bot.login(process.env.BOT_TOKEN)
+
+bot.once('ready', () => {
+    console.log(`Bot Online: ${bot.user.tag}`)
+})
+
+bot.on('message', msg => {
+    if (msg.content === '!oi')
+        msg.reply('Esse é o meu primeiro bot!')
+})
