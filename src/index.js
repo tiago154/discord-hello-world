96dda07fad19f886770b0ce1d3d3da476492f9d6
Tiago Silva <tiago.toya@hotmail.com>
2020-03-19 14:54:05 -0300

    chore: heroku

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ const Discord = require('discord.js')
 
 const bot = new Discord.Client()
 
-bot.login(process.env.BOT_TOKEN)
+bot.login(process.env.BOT_TOKEN || 'NjkwMjE5Njc4NTU5NTAyMzUy.XnOuKg.WNdYoEtRj_kT8WwehFjvWo8DbWQ')
 
 bot.once('ready', () => {
     console.log(`Bot Online: ${bot.user.tag}`)
 })
 
 bot.on('message', msg => {
-    if (msg.content === '!oi')
+    if (msg.content === '!oi') {
         msg.reply('Esse é o meu primeiro bot!')
+        msg.channel.send('Esse é o teste')
+    }
 })
